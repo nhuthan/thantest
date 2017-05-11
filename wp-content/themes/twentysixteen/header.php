a239f335153a91fe1bd0f1cd08b7af3bd15ebbe3
zoogi <phannhuthan@gmail.com>
2017-05-11 16:14:10 +0700

    change

--- wp-content/themes/twentysixteen/header.php
+++ wp-content/themes/twentysixteen/header.php
@@ -22,7 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-fsdkjfklsdjflkjsdlfjlsdjl
+fsdkjfklsdjflkjsdlfjlsdjl 
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
