5ec78b98094bca70544495fe14131418173278af
zoogi <phannhuthan@gmail.com>
2017-05-11 16:32:33 +0700

    ff

--- wp-content/themes/twentysixteen/header.php
+++ wp-content/themes/twentysixteen/header.php
@@ -22,7 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>   
-fsdkjfklsdjflkjsdlfjlsdjl    dfd
+fsdkjfklsdjflkjsdlfjlsdjl    dfd fđ
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
